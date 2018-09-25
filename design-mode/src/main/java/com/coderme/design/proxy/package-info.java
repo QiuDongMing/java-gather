9490d1726fb5cde786bb46e89a77f3bfe448ae05
qiudongming <qiudm@dachentech.com.cn>
2018-09-25 18:44:08 +0800

    新增cglib 和 jdk 动态代理区别

--- design-mode/src/main/java/com/coderme/design/proxy/package-info.java
+++ design-mode/src/main/java/com/coderme/design/proxy/package-info.java
@@ -12,4 +12,10 @@ package com.coderme.design.proxy;
  * 静态代理是由程序员创建或特定工具自动生成源代码，在对其编译。在程序员运行之前，代理类.class文件就已经被创建了。
  * 动态代理是在程序运行时通过反射机制动态创建的。
  *
+ *
+ * JDK动态代理是通过接口中的方法名，在动态生成的代理类中调用业务实现类的同名方法；
+ * CGlib动态代理是通过继承业务类，生成的动态代理类是业务类的子类，通过重写业务方法进行代理；1
+ * https://www.ibm.com/developerworks/cn/java/j-lo-proxy-pattern/
+ *
+ *
  */
